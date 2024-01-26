210590cca4acaf33af88cfaaa20a3373423f0759
malikmsohaib <sobimalik9@gmail.com>
2024-01-26 21:12:09 +0500

    test

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,7 @@
-import json
+import pandas as pd
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    d = {'col1': [1,2], 'col2': [3,4]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    print('Done x1.1')
